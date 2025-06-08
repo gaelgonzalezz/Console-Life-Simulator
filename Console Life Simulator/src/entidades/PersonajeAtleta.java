d247525deda0e9f411a8b723ddd4452c6399e6d6
Gael Gonzalez <gaelox162@gmail.com>
2025-06-07 23:48:35 -0300

    Add files via upload

---
+++ Console Life Simulator/src/entidades/PersonajeAtleta.java
@@ -0,0 +1,37 @@
+package entidades;
+
+public class PersonajeAtleta extends Personaje {
+	 public PersonajeAtleta(String nombre) {
+	        super(nombre, "Atleta");
+	        this.vida = 120; 
+	        this.felicidad = 90;
+	    }
+	    
+	    @Override
+	    public void habilidadEspecial() {
+	        System.out.println("🏃‍♂️ ¡HABILIDAD ATLETA: Entrenamiento Intensivo!");
+	        System.out.println("Ganas mucha más condición física y felicidad, y se reducen los efectos contraproducentes.");
+	        this.vida = limitarValor(this.vida + 15);
+	        this.felicidad = limitarValor(this.felicidad + 20);
+	        this.hambre = limitarValor(this.hambre + 10);
+	        this.sueno = limitarValor(this.sueno + 5);
+	        this.suciedad = limitarValor(this.suciedad + 20);
+	    }
+	    
+	    @Override
+	    public void ejercitarse() {
+	    	System.out.println("💪 ¡Entrenamiento de atleta profesional!");
+	        this.vida = limitarValor(this.vida + 8); // Más beneficios
+	        this.felicidad = limitarValor(this.felicidad + 25);
+	        this.hambre = limitarValor(this.hambre + 15); // Menos hambre
+	        this.sueno = limitarValor(this.sueno + 10); // Menos cansancio
+	        this.suciedad = limitarValor(this.suciedad + 30);
+	        sumarAccionFavorita();
+	    }
+	    
+	    @Override
+	    public String getDescripcionTipo() {
+	        return "Atleta profesional con gran resistencia física y amor por el ejercicio.";
+	    }
+
+}
---
+++ Console Life Simulator/src/entidades/PersonajeIntelectual.java
@@ -0,0 +1,47 @@
+package entidades;
+
+public class PersonajeIntelectual extends Personaje {
+    
+    public PersonajeIntelectual(String nombre) {
+        super(nombre, "Intelectual");
+        this.inteligencia = 150;
+        this.dinero = 800;
+        this.felicidad = 80;
+    }
+    
+    @Override
+    public void habilidadEspecial() {
+        System.out.println("🧠 ¡HABILIDAD INTELECTUAL: Momento Eureka!");
+        System.out.println("Tienes una idea brillante que te hace ganar dinero e inteligencia.");
+        this.inteligencia += 10;
+        this.dinero += 200;
+        this.felicidad = limitarValor(this.felicidad + 30);
+        this.sueno = limitarValor(this.sueno + 20);
+    }
+    
+    @Override
+    public void estudiar() {
+        System.out.println("📚 ¡Sesión de estudio intensiva!");
+        this.inteligencia += 5;
+        this.felicidad = limitarValor(this.felicidad + 15); 
+        this.sueno = limitarValor(this.sueno + 8); 
+        this.hambre = limitarValor(this.hambre + 3);
+        this.suciedad = limitarValor(this.suciedad + 5); //Mas suciedad por estar concentrado
+    }
+    
+    @Override
+    public void trabajar() {
+        System.out.println("💼 ¡Trabajo intelectual de alto nivel!");
+        this.dinero += 100; // Más dinero
+        this.felicidad = limitarValor(this.felicidad - 5); 
+        this.hambre = limitarValor(this.hambre + 10);
+        this.sueno = limitarValor(this.sueno + 25);
+        this.suciedad = limitarValor(this.suciedad + 5); 
+    }
+    
+    
+    @Override
+    public String getDescripcionTipo() {
+        return "Intelectual brillante que disfruta aprender y generar ideas innovadoras.";
+    }
+}
